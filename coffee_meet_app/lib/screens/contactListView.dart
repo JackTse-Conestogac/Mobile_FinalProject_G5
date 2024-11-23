7ac6adee2d818e0044ad8f2025f7da8d8e82276c
CSchoen1228 <cschoen1228@conestogac.on.ca>
2024-11-23 13:14:50 -0500

    Added scaffold to ListViews

--- coffee_meet_app/lib/screens/contactListView.dart
+++ coffee_meet_app/lib/screens/contactListView.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import '../listTiles/contactTile.dart';
 
-class ContactListView extends StatelessWidget{
+class ContactListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Text("TEMP");
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(text),
+        ),
+        body: ListView.builder(
+            itemCount: 1,
+            itemBuilder: (context, index) {
+              return ContactTile();
+            }));
   }
+
   // It made sense to set the tabs stuff here
   static const icon = Icons.contacts;
   static const text = "Contacts";
--- coffee_meet_app/lib/screens/eventListView.dart
+++ coffee_meet_app/lib/screens/eventListView.dart
@@ -4,7 +4,15 @@ class EventListView extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Text("TEMP");
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(text),
+        ),
+        body: ListView.builder(
+            itemCount: 1,
+            itemBuilder: (context, index) {
+              return ListTile();
+            }));;
   }
   // It made sense to set the tabs stuff here
   static const icon = Icons.edit_calendar;
