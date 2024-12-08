e0b2b9639bed865adc996b7b7e9b0671d91070f2
CSchoen1228 <cschoen1228@conestogac.on.ca>
2024-12-08 15:55:10 -0500

    Map Stuff

--- coffee_meet_app/lib/entities/global_state.dart
+++ coffee_meet_app/lib/entities/global_state.dart
@@ -1,11 +1,15 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 import 'User.dart';
 
 class GlobalState{
   static final GlobalState _instance = GlobalState.interal();
   late User _currentUser;
+  late LatLng _location;
 
 GlobalState.interal(){
     _currentUser = User(id: 0, name: "name", password: "password", email: "email", phone: 123, occupation: "occupation");
+    _location = LatLng(0, 0);
   }
 
   factory GlobalState(){
@@ -20,4 +24,12 @@ GlobalState.interal(){
   User getCurrentUser(){
     return _currentUser;
   }
+
+  void setLocation(LatLng location){
+    _location = location;
+  }
+  LatLng getLocation(){
+   return _location;
+  }
+
 }
--- coffee_meet_app/lib/screens/map_screen.dart
+++ coffee_meet_app/lib/screens/map_screen.dart
@@ -1,8 +1,10 @@
+import "package:coffee_meet_app/entities/global_state.dart";
 import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
+import "package:geolocator/geolocator.dart";
 import "package:google_maps_flutter/google_maps_flutter.dart";
 
-class MapScreen extends StatefulWidget{
+class MapScreen extends StatefulWidget {
   @override
   State<MapScreen> createState() => _MapScreenState();
 }
@@ -10,18 +12,87 @@ class MapScreen extends StatefulWidget{
 class _MapScreenState extends State<MapScreen> {
   late GoogleMapController mapController;
 
+  List<Marker> markers = [
+    Marker(
+        markerId: MarkerId("gps_ghost"),
+        infoWindow: InfoWindow(title: "Gps Ghost"),
+        position: LatLng(37.422, -122.084),
+    ),
+    Marker(
+        markerId: MarkerId("water"),
+        infoWindow: InfoWindow(title: "Some Water"),
+        position: LatLng(37.475, -122.084)
+    ),
+    Marker(
+      markerId: MarkerId("mountain"),
+      infoWindow: InfoWindow(title: "Some Mountain"),
+      position: LatLng(37.302, -122.184),
+    )
+  ];
+
+  Future<void> _locate() async{
+
+    bool locationServicesEnabled = await Geolocator.isLocationServiceEnabled();
+    if(!locationServicesEnabled){
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("Location Services Disabled"))
+      );
+      return;
+    }
+    LocationPermission permission = await Geolocator.checkPermission();
+    if(permission==LocationPermission.denied){
+      await Geolocator.requestPermission();
+      permission = await Geolocator.checkPermission();
+    }
+    if(permission != LocationPermission.denied&& permission!=LocationPermission.deniedForever){
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("Please Wait"))
+      );
+
+      Position p = await Geolocator.getCurrentPosition();
+      setState(() {
+        GlobalState().setLocation(LatLng(p.latitude,p.longitude));
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("${GlobalState().getLocation()}"))
+      );
+
+      mapController.moveCamera(CameraUpdate.newCameraPosition(
+        CameraPosition(target: GlobalState().getLocation(),zoom: 11)
+      ));
+    }else{
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("Permissions Denied"))
+      );
+    }
+  }
+
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+    setState(() {
+
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton:FloatingActionButton(
+          onPressed: _locate,
+          child: Center(child: Text("Locate")),
+      ),
       appBar: AppBar(
         title: Text("Map"),
       ),
       body: Container(
-        child: GoogleMap(onMapCreated: _onMapCreated,initialCameraPosition: CameraPosition(target: LatLng(-33.86, 151.20), zoom:11.0)),
+        child: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition:
+              CameraPosition(target: GlobalState().getLocation() ?? LatLng(0,0), zoom: 11.0),
+          markers: Set<Marker>.of(markers) ,
+        ),
       ),
     );
   }
